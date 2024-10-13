eda17f81e3ba55b45a7709517a8364c79671be02
YOGANAND2003 <jammisettyyoganand@gmail.com>
2024-10-14 02:34:44 +0530

    set

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: 'dist', // Ensure the output directory is set
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit
+  },
+});
